0da698896b9e1318f4568726c570396a7ce6ac97
Abiramikrishnaraj <43010555+Abiramikrishnaraj@users.noreply.github.com>
2018-10-03 02:30:23 -0300

    Add files via upload

---
+++ 1task2.py
@@ -0,0 +1,15 @@
+
+#Task-2parta
+file=open("running-config.cfg.txt")
+list=[]
+for line in file:
+	line=line.strip()
+	line=line.split()
+	if (line[0]=="security-level"):
+		line[1]=10
+		print("security-level",line[1])
+	if (line[0]=="ip" and line[1]=="address"):
+		x=line[2].split(".")
+		x[0]=10
+		print(x[0:])
+
